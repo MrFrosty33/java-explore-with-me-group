282ba375d6386577d46116ef0625736aea554efb
Alexander.Ivanov <alexander.ivanov@vrpconsulting.com>
2025-06-25 19:09:09 +0400

    Added template classes and property files

---
+++ main-service/src/main/java/ewm/MainApp.java
@@ -0,0 +1,7 @@
+package ewm;
+
+public class MainApp {
+    public static void main(String[] args) {
+
+    }
+}
---
+++ stat/stat-service/src/main/java/ewm/StatApp.java
@@ -0,0 +1,7 @@
+package ewm;
+
+public class StatApp {
+    public static void main(String[] args) {
+
+    }
+}
