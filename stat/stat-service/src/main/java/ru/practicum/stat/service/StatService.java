fd0981a3716d9cf5938c30edf65599409da3e1af
yalozok <olga.kozmovskaia@yandex.ru>
2025-06-27 21:16:02 +0200

    Extract Interface (correct commit)

--- stat/stat-service/src/main/java/ru/practicum/stat/service/StatService.java
+++ stat/stat-service/src/main/java/ru/practicum/stat/service/StatService.java
@@ -1,26 +1,13 @@
 package ru.practicum.stat.service;
 
-import lombok.RequiredArgsConstructor;
-import org.springframework.stereotype.Service;
 import ru.practicum.stat.dto.EndpointHitCreate;
 import ru.practicum.stat.dto.ViewStats;
 
 import java.time.LocalDateTime;
 import java.util.List;
 
-@Service
-@RequiredArgsConstructor
-public class StatService {
-    private final StatRepository statRepository;
+public interface StatService {
+    void saveHit(EndpointHitCreate hitCreate);
 
-    public void saveHit(EndpointHitCreate hitCreate) {
-        statRepository.save(hitCreate);
-    }
-
-    public List<ViewStats> getStats(LocalDateTime start, LocalDateTime end, List<String> uris, boolean unique) {
-        if (unique) {
-            return statRepository.findUniqueHits(start, end, uris);
-        }
-        return statRepository.findAllHits(start, end, uris);
-    }
+    List<ViewStats> getStats(LocalDateTime start, LocalDateTime end, List<String> uris, boolean unique);
 }
