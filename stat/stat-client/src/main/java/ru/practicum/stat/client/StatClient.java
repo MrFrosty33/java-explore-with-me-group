e6f58d03491173b949fc4b3db66894ce9e2593de
Frosty-WinPC <al.korobko@yahoo.com>
2025-06-27 14:59:42 +0200

    remove validation, fix url in getStats. methods are now returning ResponseEntity<>

--- stat/stat-client/src/main/java/ru/practicum/stat/client/StatClient.java
+++ stat/stat-client/src/main/java/ru/practicum/stat/client/StatClient.java
@@ -1,5 +1,7 @@
 package ru.practicum.stat.client;
 
+import org.apache.coyote.Response;
+import org.springframework.http.ResponseEntity;
 import ru.practicum.stat.dto.EndpointHitCreate;
 import ru.practicum.stat.dto.ViewStats;
 
@@ -7,7 +9,7 @@ import java.time.LocalDateTime;
 import java.util.List;
 
 public interface StatClient {
-    void createHit(EndpointHitCreate endpointHitCreate);
+    ResponseEntity<Void> createHit(EndpointHitCreate endpointHitCreate);
 
-    ViewStats getStats(LocalDateTime start, LocalDateTime end, List<String> uris, boolean unique);
+    ResponseEntity<List<ViewStats>> getStats(LocalDateTime start, LocalDateTime end, List<String> uris, boolean unique);
 }
--- stat/stat-client/src/main/java/ru/practicum/stat/client/StatClientImpl.java
+++ stat/stat-client/src/main/java/ru/practicum/stat/client/StatClientImpl.java
@@ -1,15 +1,13 @@
 package ru.practicum.stat.client;
 
-import jakarta.validation.Valid;
-import jakarta.validation.constraints.NotBlank;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.core.ParameterizedTypeReference;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
-import org.springframework.validation.annotation.Validated;
 import org.springframework.web.client.RestClient;
 import org.springframework.web.util.UriComponentsBuilder;
 import ru.practicum.stat.dto.EndpointHitCreate;
@@ -19,7 +17,6 @@ import java.time.LocalDateTime;
 import java.util.List;
 
 @RequiredArgsConstructor(onConstructor_ = @Autowired)
-@Validated
 @Slf4j
 @Service
 public class StatClientImpl implements StatClient {
@@ -33,56 +30,53 @@ public class StatClientImpl implements StatClient {
                 .build();
     }
 
-    // Насчёт валидации я не уверен, стоит ли её делать в клиенте или же делегировать это на контроллер
-    // с одной стороны, если здесь уже отсеивать, то не придётся делать лишних вызовов к серверу
-    // с другой стороны, будто бы это должно быть работой контроллера.
-    // Да и вообще, нужна ли валидация, если это внутренний сервис, запросы в который поступают только от main-service?
-    // Можно просто понадеяться, что сюда поступают уже корректные данные и опустить валидацию, оставить только логи
-
-    public void createHit(@Valid EndpointHitCreate endpointHitCreate) {
+    public ResponseEntity<Void> createHit(EndpointHitCreate endpointHitCreate) {
         log.trace("StatClient: createHit() call with endpointHitCreate body: {}", endpointHitCreate);
 
         String url = serverUrl + "/hit";
 
-        ResponseEntity<String> result = client
+        ResponseEntity<Void> result = client
                 .post()
                 .uri(url)
                 .contentType(MediaType.APPLICATION_JSON)
                 .body(endpointHitCreate)
                 .retrieve()
-                .toEntity(String.class);
+                .toEntity(Void.class);
 
         if (!result.getStatusCode().is2xxSuccessful()) {
             log.info("StatClient: createHit() success with status: {}, body: {}, serverUrl: {}",
                     result.getStatusCode(), result.getBody(), url);
         } else {
-            log.warn("StatClient: createHit() failure with status: {}, body: {}, serverUrl: {}",
-                    result.getStatusCode(), result.getBody(), url);
+            log.warn("StatClient: createHit() failure with status: {}, serverUrl: {}",
+                    result.getStatusCode(), url);
         }
+
+        return result;
     }
 
-    public ViewStats getStats(@NotBlank(message = "must not be null") LocalDateTime start,
-                              @NotBlank(message = "must not be null") LocalDateTime end,
-                              List<String> uris,
-                              boolean unique) {
+    public ResponseEntity<List<ViewStats>> getStats(LocalDateTime start,
+                                                    LocalDateTime end,
+                                                    List<String> uris,
+                                                    boolean unique) {
         log.trace("StatClient: getStats() call with params: start={}, end={}, uris={}, unique={}",
                 start, end, uris, unique);
 
         String url = UriComponentsBuilder
-                .fromHttpUrl(serverUrl)
+                .fromHttpUrl(serverUrl + "/stats")
                 .queryParam("start", start)
                 .queryParam("end", end)
                 .queryParam("uris", uris)
                 .queryParam("unique", unique)
                 .toUriString();
 
-        ResponseEntity<ViewStats> result = client
+        ResponseEntity<List<ViewStats>> result = client
                 .get()
                 .uri(url)
                 .retrieve()
-                .toEntity(ViewStats.class);
+                .toEntity(new ParameterizedTypeReference<List<ViewStats>>() {
+                });
 
-        if (!result.getStatusCode().is2xxSuccessful()) {
+        if (result.getStatusCode().is2xxSuccessful()) {
             log.info("StatClient: getStats() success with status: {}, body: {}, serverUrl: {}",
                     result.getStatusCode(), result.getBody(), url);
         } else {
@@ -90,7 +84,7 @@ public class StatClientImpl implements StatClient {
                     result.getStatusCode(), result.getBody(), url);
         }
 
-        return result.getBody();
+        return result;
     }
 
 }
