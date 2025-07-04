f51604cfe93685aad2058f410ff69d15d9795b50
Frosty-WinPC <al.korobko@yahoo.com>
2025-07-04 15:20:29 +0200

    sync with main_svc

---
+++ main-service/src/main/java/ru/practicum/explore/with/me/repository/EventPrivateRepository.java
@@ -0,0 +1,19 @@
+package ru.practicum.explore.with.me.repository;
+
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+import ru.practicum.explore.with.me.model.User;
+import ru.practicum.explore.with.me.model.event.Event;
+
+public interface EventPrivateRepository extends JpaRepository<Event, Long> {
+    @Query("""
+            SELECT DISTINCT e FROM Event e
+            WHERE e.initiator = :user
+            ORDER BY e.createdOn
+            """)
+    Page<Event> findEventsByUserWitOffsetAndLimit(@Param("user") User user,
+                                                  Pageable pageable);
+}
