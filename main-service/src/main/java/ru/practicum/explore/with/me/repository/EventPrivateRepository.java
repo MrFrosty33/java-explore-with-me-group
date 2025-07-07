570c4d6b82a3ff7b1896af77096eecfd14ccbb90
yalozok <olga.kozmovskaia@yandex.ru>
2025-07-07 15:47:41 +0200

    Add Controller, Service, Repository

---
+++ main-service/src/main/java/ru/practicum/explore/with/me/repository/EventPrivateRepository.java
@@ -0,0 +1,21 @@
+package ru.practicum.explore.with.me.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+import ru.practicum.explore.with.me.model.User;
+import ru.practicum.explore.with.me.model.event.Event;
+
+import java.util.List;
+
+
+public interface EventPrivateRepository extends JpaRepository<Event, Long> {
+    @Query("""
+            SELECT DISTINCT e FROM Event e
+            WHERE e.initiator = :user
+            ORDER BY e.eventDate ASC LIMIT :size OFFSET :from
+            """)
+    List<Event> findEventsByUserWitOffsetAndLimit(@Param("user") User user,
+                                                  @Param("from") int from,
+                                                  @Param("size") int size);
+}
---
+++ main-service/src/main/java/ru/practicum/explore/with/me/service/EventsPrivateService.java
@@ -0,0 +1,69 @@
+package ru.practicum.explore.with.me.service;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+import ru.practicum.explore.with.me.mapper.EventMapper;
+import ru.practicum.explore.with.me.model.Category;
+import ru.practicum.explore.with.me.model.User;
+import ru.practicum.explore.with.me.model.event.Event;
+import ru.practicum.explore.with.me.model.event.EventFullDto;
+import ru.practicum.explore.with.me.model.event.EventShortDto;
+import ru.practicum.explore.with.me.model.event.EventState;
+import ru.practicum.explore.with.me.model.event.NewEventDto;
+import ru.practicum.explore.with.me.repository.CategoryRepository;
+import ru.practicum.explore.with.me.repository.EventPrivateRepository;
+import ru.practicum.explore.with.me.repository.UserRepository;
+
+import java.util.List;
+
+@Service
+@RequiredArgsConstructor
+@Slf4j
+public class EventsPrivateService {
+    private final EventPrivateRepository eventRepository;
+    private final UserRepository userRepository;
+    private final CategoryRepository categoryRepository;
+    private final EventMapper eventMapper;
+
+    @Transactional
+    public EventFullDto createEvent(long userId, NewEventDto eventDto) {
+        User user = userRepository.findById(userId).orElse(null);
+                //.orElseThrow(
+                // () -> new NotFoundException("The required object was not found.",
+                //        "User with id=" + id + " was not found");
+        //);
+        Category category = categoryRepository.findById(eventDto.getCategory()).orElse(null);
+                //.orElseThrow(
+                // () -> new NotFoundException("The required object was not found.",
+                //        "Category with id=" + id + " was not found");
+        //);
+        Event event = eventMapper.toModel(eventDto);
+        event.setInitiator(user);
+        event.setCategory(category);
+        event.setState(EventState.PENDING);
+        Event eventSaved = eventRepository.save(event);
+        return eventMapper.toFullDto(eventSaved);
+    }
+
+    public EventFullDto getEventById(long userId, long eventId) {
+        User user = userRepository.findById(userId).orElse(null);
+        //.orElseThrow(
+        // () -> new NotFoundException("The required object was not found.",
+        //        "User with id=" + id + " was not found");
+        //);
+        Event event = eventRepository.findById(eventId).orElse(null);
+        //.orElseThrow(
+        // () -> new NotFoundException("The required object was not found.",
+        //        "Event with id=" + id + " was not found");
+        //);
+        if (event.getInitiator().getId() != userId) {
+            log.info("User with id {} doesn't have an event with id {}", userId, eventId);
+            //throw new ConflictException("The name of category should be unique.",
+            //          "Category with name=" + name + " is already exist");
+        }
+        return eventMapper.toFullDto(event);
+    }
+}
