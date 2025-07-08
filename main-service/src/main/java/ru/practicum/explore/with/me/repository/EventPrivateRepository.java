0f6647a1a9080c62e3fc86e8bfc8a31da2c1c71d
yalozok <olga.kozmovskaia@yandex.ru>
2025-07-08 09:03:33 +0200

    Add pageable

--- main-service/src/main/java/ru/practicum/explore/with/me/repository/EventPrivateRepository.java
+++ main-service/src/main/java/ru/practicum/explore/with/me/repository/EventPrivateRepository.java
@@ -6,6 +6,7 @@ import org.springframework.data.repository.query.Param;
 import ru.practicum.explore.with.me.model.User;
 import ru.practicum.explore.with.me.model.event.Event;
 
+import java.awt.print.Pageable;
 import java.util.List;
 
 
@@ -13,9 +14,8 @@ public interface EventPrivateRepository extends JpaRepository<Event, Long> {
     @Query("""
             SELECT DISTINCT e FROM Event e
             WHERE e.initiator = :user
-            ORDER BY e.eventDate ASC LIMIT :size OFFSET :from
+            ORDER BY e.createdOn
             """)
     List<Event> findEventsByUserWitOffsetAndLimit(@Param("user") User user,
-                                                  @Param("from") int from,
-                                                  @Param("size") int size);
+                                                  Pageable pageable);
 }
