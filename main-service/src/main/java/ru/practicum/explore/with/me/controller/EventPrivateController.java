b11ec0d82050c8dd6e20e5872e64ecc70acbe752
yalozok <olga.kozmovskaia@yandex.ru>
2025-07-11 12:36:57 +0200

    Update Controller, Service, Repository

--- main-service/src/main/java/ru/practicum/explore/with/me/controller/EventPrivateController.java
+++ main-service/src/main/java/ru/practicum/explore/with/me/controller/EventPrivateController.java
@@ -9,22 +9,28 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
-import ru.practicum.explore.with.me.model.event.EventFullDto;
-import ru.practicum.explore.with.me.model.event.NewEventDto;
+import ru.practicum.explore.with.me.model.event.dto.EventFullDto;
+import ru.practicum.explore.with.me.model.event.dto.EventShortDto;
+import ru.practicum.explore.with.me.model.event.dto.NewEventDto;
+import ru.practicum.explore.with.me.model.event.dto.UpdateEventUserRequest;
 import ru.practicum.explore.with.me.service.EventService;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/users/{userId}/events")
 @Slf4j
 @RequiredArgsConstructor
 @Validated
-public class EventsPrivateController {
+public class EventPrivateController {
     private final EventService eventsService;
 
     @PostMapping()
@@ -38,9 +44,26 @@ public class EventsPrivateController {
     @GetMapping("/{eventId}")
     @ResponseStatus(HttpStatus.OK)
     public EventFullDto getEventById(@PathVariable @PositiveOrZero @NotNull Long userId,
-                                     @PathVariable @Positive @NotNull Long eventId) {
+                                     @PathVariable @PositiveOrZero @NotNull Long eventId) {
         log.info("Get event {}", eventId);
         return eventsService.getEventById(userId, eventId);
     }
 
+    @PatchMapping("/{eventId}")
+    @ResponseStatus(HttpStatus.OK)
+    public EventFullDto updateEvent(@PathVariable @PositiveOrZero @NotNull Long userId,
+                                    @PathVariable @PositiveOrZero @NotNull Long eventId,
+                                    @RequestBody @Valid UpdateEventUserRequest updateEvent) {
+        log.info("Update event {}", eventId);
+        return eventsService.updateEvent(userId, eventId, updateEvent);
+    }
+
+    @GetMapping()
+    @ResponseStatus(HttpStatus.OK)
+    public List<EventShortDto> getEvents(@PathVariable @PositiveOrZero @NotNull Long userId,
+                                         @RequestParam(defaultValue = "0") @PositiveOrZero int from,
+                                         @RequestParam(defaultValue = "10") @Positive int size) {
+        log.info("Get events by user {}", userId);
+        return eventsService.getEvents(userId, from, size);
+    }
 }
