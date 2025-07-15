6ff9cb45ed8b4a5ee221832a196e105e0f322e6a
yalozok <olga.kozmovskaia@yandex.ru>
2025-07-15 11:26:25 +0200

    Update UserAdminController

--- main-service/src/main/java/ru/practicum/explore/with/me/controller/UserAdminController.java
+++ main-service/src/main/java/ru/practicum/explore/with/me/controller/UserAdminController.java
@@ -14,6 +14,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
@@ -24,7 +25,8 @@ import ru.practicum.explore.with.me.service.UserService;
 
 import java.util.List;
 
-@RestController("/admin/users")
+@RestController
+@RequestMapping("/admin/users")
 @RequiredArgsConstructor(onConstructor_ = @Autowired)
 @Slf4j
 @Validated
