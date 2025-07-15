e13ea97e05e16c433ce0e040648c1b90b27de4aa
yalozok <olga.kozmovskaia@yandex.ru>
2025-07-15 15:09:45 +0200

    Update models

--- main-service/src/main/java/ru/practicum/explore/with/me/model/category/Category.java
+++ main-service/src/main/java/ru/practicum/explore/with/me/model/category/Category.java
@@ -1,15 +1,21 @@
 package ru.practicum.explore.with.me.model.category;
 
+import jakarta.persistence.CascadeType;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
+import ru.practicum.explore.with.me.model.event.Event;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Entity
 @Table(name = "categories")
@@ -25,4 +31,8 @@ public class Category {
 
     @Column(name = "name")
     private String name;
+
+    @OneToMany(mappedBy = "category", cascade = CascadeType.ALL, orphanRemoval = true)
+    private List<Event> events = new ArrayList<>();
+
 }
