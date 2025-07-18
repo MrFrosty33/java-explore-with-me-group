3dcf0deec393be70014a294227336b256fab4e29
yalozok <olga.kozmovskaia@yandex.ru>
2025-07-18 21:40:52 +0200

    Replace StatRepository with JPA

--- stat/stat-dto/src/main/java/ru/practicum/stat/dto/ViewStats.java
+++ stat/stat-dto/src/main/java/ru/practicum/stat/dto/ViewStats.java
@@ -1,10 +1,13 @@
 package ru.practicum.stat.dto;
 
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 import lombok.RequiredArgsConstructor;
 
 @Data
-@RequiredArgsConstructor
+@AllArgsConstructor
+@NoArgsConstructor
 public class ViewStats {
     private String app;
     private String uri;
